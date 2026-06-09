f335a9e317f3f0e975133988a99b607e1d5e6982
Ranjitha <ranjithavenkadesan06@gmail.com>
2026-06-09 21:58:12 +0530

    Add LeetCode and NeetCode practice solutions

---
+++ Arrays/Two_sum.java
@@ -0,0 +1,26 @@
+/*
+Problem: Two Sum
+Platform: LeetCode
+Difficulty: Easy
+
+Time Complexity: O(n^2)
+Space Complexity: O(1)
+*/
+
+class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+
+        for (int i = 0; i < nums.length; i++) {
+
+            for (int j = i + 1; j < nums.length; j++) {
+
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+
+            }
+        }
+
+        return new int[]{};
+    }
+}
---
+++ String/Valid_anagram.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character,Integer>set=new HashMap<>();
+        if(s.length()!=t.length()) return false;
+        for(char c:s.toCharArray()){
+            set.put(c,set.getOrDefault(c,0)+1);
+        }
+        for(char c:t.toCharArray()){
+            if(!set.containsKey(c)) return false;
+            set.put(c,set.get(c)-1);
+            if(set.get(c)==0){
+                set.remove(c);
+            }
+            
+        }
+        if(set.isEmpty()){
+            return true;
+        }
+        
+        return false;
+
+    }
+    }
+    
